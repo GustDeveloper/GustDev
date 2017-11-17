5302ed53fc80b5fe104f75a02e31defbb903dc5e
vannesschancc <vannesschan1993@gmail.com>
2017-11-17 00:58:45 -0800

    ***The Event is Under Development*****

--- app/src/main/java/com/google/firebase/quickstart/database/fragment/EventListFragment.java
+++ app/src/main/java/com/google/firebase/quickstart/database/fragment/EventListFragment.java
@@ -9,7 +9,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
---
+++ app/src/main/java/com/google/firebase/quickstart/database/viewholder/EventViewHolder.java
@@ -0,0 +1,14 @@
+package com.google.firebase.quickstart.database.viewholder;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by vanne on 11/17/2017.
+ */
+
+public class EventViewHolder extends RecyclerView.ViewHolder {
+
+    public PeopleViewHolder
+    @Override
+    public void
+}
